b21d7064af5f5dc3a9f8aaf86e08839891bec66b
QED-Axel <axel.ivan9000@gmail.com>
2024-11-14 01:10:02 -0600

    Add files via upload

---
+++ Compilador NUEVO/src/compilador/Atributos.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package compilador;
+
+/**
+ *
+ * @author Axel
+ */
+public class Atributos {
+    String tipo;
+    String h;
+    String longitud;
+    boolean esArreglo;
+    
+    public Atributos () {
+        tipo = "";
+        h = "";
+        longitud = "";
+        esArreglo = false;
+    }
+}
